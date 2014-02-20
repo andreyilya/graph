07bec0f867d6aa7d3e2b7e2c25596dbcdd7da2d7
andreyilya <liniyakamnya@gmail.com>
2014-02-20 17:58:21 +0300

    repository fix

--- engine/src/main/java/graph/engine/repository/impl/CityRepositoryImpl.java
+++ engine/src/main/java/graph/engine/repository/impl/CityRepositoryImpl.java
@@ -8,5 +8,5 @@ import graph.engine.repository.jpa.JpaSupport;
  * User: a.radkov
  * Date: 20.02.14
  */
-public class CityDaoImpl extends JpaSupport<CityEntity, String> implements CityDao {
+public class CityRepositoryImpl extends JpaSupport<CityEntity, String> implements CityDao {
 }
---
+++ engine/src/test/java/graph/engine/repository/impl/CityDaoImplTest.java
@@ -0,0 +1,27 @@
+package graph.engine.repository.impl;
+
+import graph.engine.model.CityEntity;
+import graph.engine.repository.api.CityRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: user
+ * Date: 20.02.14
+ * Time: 17:41
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:applicationContext.xml"})
+public class CityDaoImplTest {
+	@Autowired
+	private CityRepository cityRepository;
+
+	@Test
+	public void testSaveCity(){
+		cityRepository.save(new CityEntity());
+	}
+}
