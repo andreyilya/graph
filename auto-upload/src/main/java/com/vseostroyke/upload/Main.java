568eb6e95d66dc88791003293f1f721ac73bad73
a.radkov <andreyradikrfe@gmail.com>
2014-05-20 18:23:52 +0300

    base ftp-apache commons

--- auto-upload/src/main/java/com/vseostroyke/upload/Main.java
+++ auto-upload/src/main/java/com/vseostroyke/upload/Main.java
@@ -1,12 +1,9 @@
 package com.vseostroyke.upload;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.net.URL;
-import java.net.URLConnection;
-import java.util.Arrays;
+import java.io.IOException;
+import org.apache.commons.net.ftp.FTPClient;
 import org.apache.commons.net.ftp.FTPFile;
+
 /**
  * User: a.radkov
  * Date: 20.05.2014
@@ -15,17 +12,24 @@ public class Main {
     public static final String LOGIN = "fftdfbzs29847";
     public static final String PASSWORD = "Q8ibY.y511((32";
 
-    public static void main(String[] args) {
-        URL url = null;
-        File folder = null;
-        try {
-            url = new URL ("ftp://" + LOGIN + ":" + PASSWORD + "@k29.hostenko.com");
-            URLConnection urlc = url.openConnection();
-            InputStream is = urlc.getInputStream();
-            //folder = new File (is);
-        } catch (Exception e) {
-            e.printStackTrace();
+    public static void main(String[] args) throws IOException {
+
+        FTPClient ftpClient = connectToFTP("k29.hostenko.com", 21, LOGIN, PASSWORD);
+        FTPFile[] listFtpFile = ftpClient.listFiles("/wp-content/uploads");
+        for (FTPFile ftpFile1 : listFtpFile) {
+            System.out.println("Name - " + ftpFile1.getName() +
+                    "Size - " + ftpFile1.getSize() +
+                    "Link - " + ftpFile1.getLink() +
+                    "Type - " + ftpFile1.getType());
         }
-        folder.listFiles();
+    }
+
+    public static FTPClient connectToFTP(String address, int port, String username, String password)
+            throws IOException {
+        FTPClient ftpClient = new FTPClient();
+        ftpClient.connect(address, port);
+        ftpClient.login(username, password);
+
+        return ftpClient;
     }
 }
