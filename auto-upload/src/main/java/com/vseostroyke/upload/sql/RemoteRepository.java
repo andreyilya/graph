5608240363c7614135e34646a1b475629fbd7e79
a.radkov <andreyradikrfe@gmail.com>
2014-05-21 18:49:54 +0300

    insert into posts

--- auto-upload/src/main/java/com/vseostroyke/upload/sql/RemoteRepository.java
+++ auto-upload/src/main/java/com/vseostroyke/upload/sql/RemoteRepository.java
@@ -14,9 +14,12 @@ import com.vseostroyke.upload.http.ContentItem;
 public class RemoteRepository {
 
     public void generateSql(ContentItem contentItem, String path) {
-        String sqlTemplate = "START TRANSACTION; insert into wp_posts" +
+        String sqlTemplate = "START TRANSACTION;" +
+                " insert into wp_posts" +
                 " (post_author, post_content, post_title, post_status, comment_status, ping_status,post_name,post_type) values" +
-                " (3,'%s','%s','draft','closed','closed','testing-query','post');select LAST_INSERT_ID();COMMIT;";
+                " (3,'%s','%s','draft','closed','closed','testing-query','post');" +
+                "SET @last_insert_id = LAST_INSERT_ID();"  +
+                " insert into wp_term_relationships (object_id,term_taxonomy_id) values (@last_insert_id, 2);COMMIT;";
         String sqlCode = String.format(sqlTemplate,
                 contentItem.getContent(), contentItem.getTitle());
     }
