ddbe0bf7aeede2cbb82109f0229f9ba5d969f443
a.radkov <andreyradikrfe@gmail.com>
2014-06-07 12:01:36 +0300

    image Builder

--- auto-upload/src/main/java/com/vseostroyke/upload/Main.java
+++ auto-upload/src/main/java/com/vseostroyke/upload/Main.java
@@ -28,27 +28,45 @@ public class Main {
 //        contentCopier.copy(Arrays.asList(
 //                "http://www.domovoy.by/good/id/4973/900/901"
 //                , "http://www.domovoy.by/good/id/2047/700/709"),new MebelNormalizer());
-        BufferedImage image = ImageIO.read(new File("d:\\temp\\i\\items\\podstavka_pod_cvety_4.jpg"));
+        BufferedImage image = ImageIO.read(new File("d:\\temp\\i\\items\\stol-obed_Georgia-ext.jpg"));
 
         int imageWidth  = image.getWidth();
         int imageHeight = image.getHeight();
 
         double scaleX = (double)500/imageWidth;
         double scaleY = (double)375/imageHeight;
-        AffineTransform scaleTransform = AffineTransform.getScaleInstance(scaleY, scaleY);
-        AffineTransformOp bilinearScaleOp = new AffineTransformOp(scaleTransform, AffineTransformOp.TYPE_BILINEAR);
-        image= bilinearScaleOp.filter(
-                 image,
-                new BufferedImage((int) (imageWidth * scaleY), 375, image.getType()));
+        if(scaleX>scaleY) {
+            AffineTransform scaleTransform = AffineTransform.getScaleInstance(scaleY, scaleY);
+            AffineTransformOp bilinearScaleOp = new AffineTransformOp(scaleTransform, AffineTransformOp.TYPE_BILINEAR);
+            image = bilinearScaleOp.filter(
+                    image,
+                    new BufferedImage((int) (imageWidth * scaleY), 375, image.getType()));
 
 
-        BufferedImage resizedImage = new BufferedImage(500, 375, BufferedImage.TYPE_INT_RGB);
-        Graphics2D g = resizedImage.createGraphics();
-        g.setColor(Color.WHITE);
+            BufferedImage resizedImage = new BufferedImage(500, 375, BufferedImage.TYPE_INT_RGB);
+            Graphics2D g = resizedImage.createGraphics();
+            g.setColor(Color.WHITE);
 
-        g.fillRect(0, 0, 500, 375);
-        g.drawImage(image, (500-(int)(imageWidth*scaleY))/2, 0, (int)(imageWidth*scaleY), 375, null);
-        ImageIO.write(resizedImage, "jpg", new File("d:\\temp\\i\\items\\podstavka_pod_cvety_4-small.jpg"));
+            g.fillRect(0, 0, 500, 375);
+            g.drawImage(image, (500 - (int) (imageWidth * scaleY)) / 2, 0, (int) (imageWidth * scaleY), 375, null);
+            ImageIO.write(resizedImage, "jpg", new File("d:\\temp\\i\\items\\podstavka_pod_cvety_4-small.jpg"));
+        }else{
+            AffineTransform scaleTransform = AffineTransform.getScaleInstance(scaleX, scaleX);
+            AffineTransformOp bilinearScaleOp = new AffineTransformOp(scaleTransform, AffineTransformOp.TYPE_BILINEAR);
+            image = bilinearScaleOp.filter(
+                    image,
+                    new BufferedImage(500, (int) (imageHeight * scaleX), image.getType()));
+
+
+            BufferedImage resizedImage = new BufferedImage(500, 375, BufferedImage.TYPE_INT_RGB);
+            Graphics2D g = resizedImage.createGraphics();
+            g.setColor(Color.WHITE);
+
+            g.fillRect(0, 0, 500, 375);
+            g.drawImage(image, 0, (375-(int) (imageHeight * scaleX))/2, 500, (int) (imageHeight * scaleX), null);
+            ImageIO.write(resizedImage, "jpg", new File("d:\\temp\\i\\items\\stol-obed_Georgia-ext-small.jpg"));
+
+        }
 
     }
 
